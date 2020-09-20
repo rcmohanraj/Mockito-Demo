d3803f04d1f8bf3b83e9f8d96fe24beaff681a0f
mohanrajrc <mohanrajksrct@gmail.com>
2020-09-20 22:06:54 +0530

    Used Mock Annotation to create mock

--- src/test/java/com/rcmcode/interactiontest/classtypes/ConcreteClassTest.java
+++ src/test/java/com/rcmcode/interactiontest/classtypes/ConcreteClassTest.java
@@ -2,26 +2,31 @@ package com.rcmcode.interactiontest.classtypes;
 
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.MockitoAnnotations;
 
 class ConcreteClassTest {
 
+    @Mock
+    private ConcreteClass concreteClassMock;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     void testFinalMethod() {
-        ConcreteClass concreteClassMock = Mockito.mock(ConcreteClass.class);
         concreteClassMock.finalMethod();
         Mockito.verify(concreteClassMock).finalMethod();
     }
 
     @Test
     void testInstanceMethod() {
-        ConcreteClass concreteClassMock = Mockito.mock(ConcreteClass.class);
         concreteClassMock.instanceMethod();
         Mockito.verify(concreteClassMock).instanceMethod();
     }
